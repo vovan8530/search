76a35b6b36d15544ee91afed0ee56df050ef55d9
vovan8530 <vovan.b8530@gmail.com>
2022-06-26 03:50:58 +0300

    Change Event

--- app/Events/SearchProcessedEvent.php
+++ app/Events/SearchProcessedEvent.php
@@ -3,7 +3,6 @@
 namespace App\Events;
 
 use App\Models\User;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
